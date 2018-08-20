31265e3b521c73d173b703b32aec58e8e2bf7b1b
Rumi <xuhaijinsky2008@163.com>
2018-08-20 10:46:38 +0800

    add index.pum

--- tests/test.js
+++ tests/test.js
@@ -9,15 +9,14 @@ const request = supertest(app);
 describe('Tests app', function() {
   it('verifies get', function(done) {
     request.get('/').expect(200).end(function(err, result) {
-        test.string(result.body.Output).contains('Hello');
-        test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
+        test.string(result.res.text).contains('首页');
+        test.value(result).hasHeader('content-type', 'text/html; charset=utf-8');
         done(err);
     });
   });
   it('verifies post', function(done) {
-    request.post('/').expect(200).end(function(err, result) {
-        test.string(result.body.Output).contains('Hello');
-        test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
+    request.post('/').expect(404).end(function(err, result) {
+        test.value(result).hasHeader('content-type', 'text/html; charset=utf-8');
         done(err);
     });
   });
